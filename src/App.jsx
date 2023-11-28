ed3392a504e8d538b2c68106e138ae45e66a2cf7
cheolminJOO <cheolmin.joo@gmail.com>
2023-11-28 18:04:28 +0900

    merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,8 @@
-import React from 'react'
-<<<<<<< HEAD
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './components/Home/Home'
-import Detalie from './components/Detalie/Detalie'
-import NewPost from './components/NewPost/NewPost'
-import SignIn from './components/SignIn/SignIn'
-import Signup from './components/Signup/Signup'
-import Layout from './components/Layout/Layout'
-import Login from './components/Login/Login' // 이 부분 나중에 빼기
 
-function Router() {
-    const isLoggedIn = true
-
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route path="/" element={isLoggedIn ? <Home /> : <Login />} />
-                    <Route path="/posts" element={<Detalie />} />
-                    <Route path="/posts/new" element={<NewPost />} />
-                    <Route path="/login" element={<SignIn />} />
-                    <Route path="/signup" element={<Signup />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    )
-}
-
-export default Router
-=======
 import Router from './router/route'
 function App() {
     return <Router />
 }
 
 export default App
->>>>>>> c0c96609b0685ed15d3b62f86c619be61f488020
+
